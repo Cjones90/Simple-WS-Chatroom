ab28acec5807f62bd4c1ce6ea102819ab76fa8b3
KC <kcjones.2002@gmail.com>
2016-02-11 21:04:46 -0800

    fix index

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const http = require('http')
+const fs = require('fs');
 const wsServer = require("./ws.js")
 const port = process.env.PORT || 8080;
 
@@ -9,13 +10,15 @@ module.exports = {
 		console.log("Server started")
     wsServer.init();
     http.createServer((req, res) => {
+			console.log("Http req")
       let input = '';
       req.on('data', (buffer) => {
         input += buffer.toString();
       })
       req.on('end', () => {
         res.setHeader('Access-Control-Allow-Origin', '*')
-        res.end();
+				res.setHeader('content-type', 'text/html')
+        res.end(fs.readFileSync('index.html'));
       })
 
     }).listen(port, console.log(`Listening on port ${port}`))
